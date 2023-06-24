994eebf88f9fa1b1f4c3e7317ee2f5a007bad148
Piyush Pardeshi <82027371+PiyushPb@users.noreply.github.com>
2023-06-24 15:11:43 +0530

    login and signup page added

---
+++ login.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/loginandsignup.css">
+
+</head>
+<body>
+
+    <section class="container forms">
+        <div class="form login">
+            <div class="form-content">
+                <header>Login</header>
+                <form method="POST" autocomplete="off">
+                    <div class="field input-field">
+                        <input type="email" placeholder="Email" class="input" name = "email"  autocomplete="off">
+                    </div>
+
+                    <div class="field input-field">
+                        <input type="password" placeholder="Password" class="password" name = "password" autocomplete="off">
+                        <i class='bx bx-hide eye-icon'></i>
+                    </div>
+
+                    <div class="form-link">
+                        <a href="#" class="forgot-pass">Forgot password?</a>
+                    </div>
+
+                    <div class="field button-field">
+                        <button name = "login" type = "submit">Login</button>
+                    </div>
+                </form>
+
+                <div class="form-link">
+                    <span>Don't have an account? <a href="signup.php" class="link signup-link">Signup</a></span>
+                </div>
+            </div>
+
+            <div class="line"></div>
+
+            <div class="media-options">
+                <a href="" class="field facebook">
+                    <img src="assets/facebook.png" alt="" class="google-img">
+                    <span>Login with Facebook</span>
+                </a>
+            </div>
+
+            <div class="media-options">
+                <a href="#" class="field google">
+                    <img src="assets/google.png" alt="" class="google-img">
+                    <span>Login with Google</span>
+                </a>
+            </div>
+
+        </div>
+    </section>
+    
+</body>
+</html>
+
+<?php 
+    include 'conn.php';
+
+    if(isset($_POST['login'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $email_search = "select * from users where email = '$email'";
+        $query = mysqli_query($conn, $email_search);
+
+        $email_count = mysqli_num_rows($query);
+
+        if($email_count){
+            $email_pass = mysqli_fetch_assoc($query);
+
+            $db_pass = $email_pass['password'];
+
+            $_SESSION['username'] = $email_pass['name'];
+
+            $pass_decode = password_verify($password, $db_pass);
+
+            if($pass_decode){
+                ?>
+                <script>
+                    location.replace("index.php");
+                </script>
+                <?php
+            }else{
+                echo "<script>alert('Incorrect password')</script>";
+            }
+        }else{
+            echo "<script>alert('Invalid email')</script>";
+        }
+    }
+?>
---
+++ signup.php
@@ -0,0 +1,101 @@
+<?php include 'conn.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+    <link rel="stylesheet" href="css/loginandsignup.css">
+</head>
+<body>
+    <section class="container forms">
+        <div class="form signup">
+            <div class="form-content">
+                <header>Signup</header>
+                <form method="POST">
+
+                    <div class="field input-field">
+                        <input type="text" placeholder="Name" class="input" name="name">
+                    </div>
+
+                    <div class="field input-field">
+                        <input type="tel" placeholder="Phone number" class="input" name="phone">
+                    </div>
+
+                    <div class="field input-field">
+                        <input type="email" placeholder="Email" class="input" name="email">
+                    </div>
+
+                    <div class="field input-field">
+                        <input type="password" placeholder="Create password" class="password" name="password">
+                    </div>
+
+                    <div class="field input-field">
+                        <input type="password" placeholder="Confirm password" class="password" name="confirm_password">
+                        <i class='bx bx-hide eye-icon'></i>
+                    </div>
+
+                    <div class="field button-field">
+                        <button type="submit" name="signup">Signup</button>
+                    </div>
+                </form>
+
+                <div class="form-link">
+                    <span>Already have an account? <a href="login.php" class="link login-link">Login</a></span>
+                </div>
+            </div>
+
+            <div class="line"></div>
+
+            <div class="media-options">
+                <a href="assets/facebook.png" class="field facebook">
+                    <img src="assets/facebook.png" alt="" class="google-img">
+                    <span>Login with Facebook</span>
+                </a>
+            </div>
+
+            <div class="media-options">
+                <a href="#" class="field google">
+                    <img src="assets/google.png" alt="" class="google-img">
+                    <span>Login with Google</span>
+                </a>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
+
+<?php
+
+if (isset($_POST['signup'])) {
+    $name = $_POST['name'];
+    $phone_number = $_POST['phone'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    if (empty($name) || empty($phone_number) || empty($email) || empty($password) || empty($confirm_password)) {
+        echo "<script>alert('Please fill in all fields')</script>";
+    } elseif ($password != $confirm_password) {
+        echo "<script>alert('Passwords do not match')</script>";
+    } else {
+        $query = "SELECT * FROM users WHERE email = '$email' OR phone_number = '$phone_number'";
+        $result = $conn->query($query);
+
+        if ($result->num_rows > 0) {
+            echo "<script>alert('User email or phone number already registered')</script>";
+        } else {
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            $sql = "INSERT INTO users (name, email, phone_number, password) VALUES ('$name', '$email', '$phone_number', '$hashed_password')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "<script>alert('Data uploaded successfully')</script>";
+            } else {
+                echo "<script>alert('Error uploading data: " . $conn->error . "')</script>";
+            }
+        }
+    }
+}
+?>
+
