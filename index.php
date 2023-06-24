023a10c7efb2ab844d2c2c98475ad467e57999df
Piyush Pardeshi <piyushp0512@gmail.com>
2023-06-24 16:53:25 +0530

    home page (services added)

--- index.php
+++ index.php
@@ -7,20 +7,98 @@
     <link rel="stylesheet" href="css/home.css">
 </head>
 <body>
-    <section class="home">
         <div class="home_bg">
             <div class="home_bg_overlay"></div>
             <div class="home_content">
                 <h2>Services on demand</h2>
                 <p>Get your services done at your doorstep</p>
                 <form method="GET" autocomplete = "off">
-                    <input type="text" id="searchInput" placeholder="Search for services" oninput="showServices(this.value)">
+                    <input type="text" id="searchInput" placeholder="Search for services" name = "service" oninput="showServices(this.value)">
                     <button type="submit">Search</button>
                 </form>
                 <div id="servicesDropdown"></div>
             </div>
         </div>
-    </section>
+
+        <div class="services_container">
+        <h2>Services</h2>
+        <p>Choose from a wide range of services</p>
+        <div class="service_card_container">
+        <div class="service_card">  
+
+            <div class="service_card_img">
+                <img src="./assets/lawyer.png" alt="plumbing">
+            </div>
+            <div class="service_card_content">
+                <h3>Lawyer</h3>
+            </div>
+            </div>  
+
+            <div class="service_card">
+            <div class="service_card_img">
+                <img src="./assets/lawyer.png" alt="plumbing">
+            </div>
+            <div class="service_card_content">
+                <h3>Lawyer</h3>
+            </div>
+            </div>  
+
+            <div class="service_card">
+            <div class="service_card_img">
+                <img src="./assets/lawyer.png" alt="plumbing">
+            </div>
+            <div class="service_card_content">
+                <h3>Lawyer</h3>
+            </div>
+            </div>  
+
+            <div class="service_card">
+            <div class="service_card_img">
+                <img src="./assets/lawyer.png" alt="plumbing">
+            </div>
+            <div class="service_card_content">
+                <h3>Lawyer</h3>
+            </div>
+            </div>  
+
+            <div class="service_card">
+            <div class="service_card_img">
+                <img src="./assets/lawyer.png" alt="plumbing">
+            </div>
+            <div class="service_card_content">
+                <h3>Lawyer</h3>
+            </div>
+            </div>  
+
+            <div class="service_card">
+            <div class="service_card_img">
+                <img src="./assets/lawyer.png" alt="plumbing">
+            </div>
+            <div class="service_card_content">
+                <h3>Lawyer</h3>
+            </div>
+            </div>  
+
+            <div class="service_card">
+            <div class="service_card_img">
+                <img src="./assets/lawyer.png" alt="plumbing">
+            </div>
+            <div class="service_card_content">
+                <h3>Lawyer</h3>
+            </div>
+            </div>  
+
+            <div class="service_card">
+            <div class="service_card_img">
+                <img src="./assets/lawyer.png" alt="plumbing">
+            </div>
+            <div class="service_card_content">
+                <h3>Lawyer</h3>
+            </div>
+            </div>  
+        </div>
+        
+        </div>
 
     <script>
         // List of available services (you can modify this as per your requirements)
